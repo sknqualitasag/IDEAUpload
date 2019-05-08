3e4fee8fa9f03aacc82069fe4379df2248fb21a6
Sophie Kunz <sophie.kunz@qualitasag.ch>
2019-05-08 09:45:52 +0200

    First XML

--- R/20190507_generate_xml.R
+++ R/20190507_generate_xml.R
@@ -9,6 +9,34 @@ s_csv_file <- file.path("inst/extdata/genotyped_animal.csv")
 df <- read.csv(file=s_csv_file, header = TRUE, sep = ";")
 
 #' Create of XML 
+library(XML)
+genoanim <- newXMLNode("interbull", attrs = c(type = "animinfo", version ="1.0"))
+animals <- newXMLNode("animals", parent = genoanim)
+a <- newXMLNode("a", attrs = c( id = "LIMIRLM12314567890"),
+           parent = animals)
+genobeef <- newXMLNode("GENO_BEEF", attrs = c(pop = "IRL", genotyped = "Y", share = "Y", tissue ="E"),
+                       parent = a)
+genoanim
+
+
+#Alternative
+newXMLNode("interbull",
+           attrs = c(type = "animinfo", version ="1.0"), newXMLNode("animals",  newXMLNode("a",
+                      attrs = c(id = "LIMIRLM12314567890")), newXMLNode("GENO_BEEF",
+                                                                        attrs = c(pop = "IRL", genotyped = "Y", share = "Y", tissue ="E")))
+)
+
+#Anwendung Beispiele mit unsere Daten
+b <- mapply(function(x)
+  newXMLNode("a",
+             attrs = c(id = x)),
+            df[, 1])
+grp <- newXMLNode("animals", .children = b)
+subTree <- newXMLNode("grp","a", "GENO_BEEF")
+
+doc = newXMLDoc()
+newXMLNode("root", parent = doc)
+
 #' https://stackoverflow.com/questions/39246686/how-to-create-xml-from-csv-properly
 #library(XML)
 #xml <- xmlTree("animals")
@@ -23,6 +51,3 @@ df <- read.csv(file=s_csv_file, header = TRUE, sep = ";")
 #xml$closeNode()
 #saveXML(xml$doc(), "animinfo.xml")
 #xmlParse("animinfo.xml")
-
-vNode = newXMLNode("GENO_BEEF",
-                   attrs = c(type = "integer", length = 3))
