6973eb862a1bbb1ffbf69812e0cefc9e7619e0fa
Sophie Kunz <sophie.kunz@qualitasag.ch>
2019-05-10 09:35:31 +0200

    Generate XML File

--- R/20190507_generate_xml.R
+++ R/20190507_generate_xml.R
@@ -11,11 +11,9 @@ df <- read.csv(file=s_csv_file, header = TRUE, sep = ";", stringsAsFactors=FALSE
 #' Create of XML 
 library(XML)
 
-#' 1. Versuch
-#' Loop funktionniert nicht, es überschreibt die vorherige Tiere
+genoanim <- newXMLNode("interbull", attrs = c(type = "animinfo", version ="1.0"))
+animals <- newXMLNode("animals", parent = genoanim)
 for (i in 1:nrow(df)) {
-  genoanim <- newXMLNode("interbull", attrs = c(type = "animinfo", version ="1.0"))
-  animals <- newXMLNode("animals", parent = genoanim)
   a <- newXMLNode("a", attrs = c( id =df[i,"id"]),
                   parent = animals)
   
@@ -25,20 +23,6 @@ for (i in 1:nrow(df)) {
     df[i,appNames[1]], df[i,appNames[2]], df[i,appNames[3]], df[i,appNames[4]])
   
 }
-genoanim
+print(genoanim)
+saveXML(genoanim, file="IDEAUploadFile.xml")
 
-
-#' 2. Versuch
-#' Ab dem Node animals ist i.O., jedoch Interbull node fehlt
-xmlt_anim <- xmlTree("animals")
- for (i in 1:nrow(df)) {
-    xmlt_anim$addNode("a", attrs = c(id = df[i,"id"]), close = FALSE)
-     appNames <- names(df)[names(df) != "id"]
-     mapply(function(w, x, y, z)
-       xmlt_anim$addNode("GENO_BEEF", attrs = c(pop = w, genotyped = x, share = y, tissue = z), close = FALSE),
-       df[i,appNames[1]], df[i,appNames[2]], df[i,appNames[3]], df[i,appNames[4]])
-     xmlt_anim$closeNode()
-    xmlt_anim$closeNode()
- }
-xmlt_anim$doc()  
-  
