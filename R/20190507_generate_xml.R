8bce81e49b01a0726df18ceb3a6c91ac54ca5a69
Sophie Kunz <sophie.kunz@qualitasag.ch>
2019-05-10 08:43:56 +0200

    2 Versuche

--- R/20190507_generate_xml.R
+++ R/20190507_generate_xml.R
@@ -7,86 +7,38 @@
 #' Read the example csv-file in inst/extdata
 s_csv_file <- file.path("inst/extdata/genotyped_animal.csv")
 df <- read.csv(file=s_csv_file, header = TRUE, sep = ";", stringsAsFactors=FALSE)
-# df$id <- as.character(as.factor(df$id))
-# df$pop <- as.character(as.factor(df$pop))
-# df$genotyped <- as.character(as.factor(df$genotyped))
-# df$share <- as.character(as.factor(df$share))
-# df$tissue <- as.character(as.factor(df$tissue))
 
 #' Create of XML 
 library(XML)
 
-#for (i in 1:nrow(df)) {
-  i <- 1
+#' 1. Versuch
+#' Loop funktionniert nicht, es überschreibt die vorherige Tiere
+for (i in 1:nrow(df)) {
   genoanim <- newXMLNode("interbull", attrs = c(type = "animinfo", version ="1.0"))
   animals <- newXMLNode("animals", parent = genoanim)
-  #a <- newXMLNode("a", attrs = c( id = "LIMIRLM12314567890"),
-  #           parent = animals)
   a <- newXMLNode("a", attrs = c( id =df[i,"id"]),
                   parent = animals)
   
-  #genobeef <- newXMLNode("GENO_BEEF", attrs = c(pop = "IRL", genotyped = "Y", share = "Y", tissue ="E"),
-  #                       parent = a)
   appNames <- names(df)[names(df) != "id"]
   genobeef <- mapply(function(w, x, y, z)
     newXMLNode("GENO_BEEF", attrs = c(pop = w, genotyped = x, share = y, tissue = z), parent = a),
     df[i,appNames[1]], df[i,appNames[2]], df[i,appNames[3]], df[i,appNames[4]])
-#}
-
-  genoanim
-
   
-  
-#------------------------------------------------------  
-#Alternative
-newXMLNode("interbull",
-           attrs = c(type = "animinfo", version ="1.0"), newXMLNode("animals",  newXMLNode("a",
-                      attrs = c(id = "LIMIRLM12314567890")), newXMLNode("GENO_BEEF",
-                                                                        attrs = c(pop = "IRL", genotyped = "Y", share = "Y", tissue ="E")))
-)
-
-#Anwendung Beispiele mit unsere Daten
-b <- mapply(function(x)
-  newXMLNode("a",
-             attrs = c(id = x)),
-            df[, 1])
-grp <- newXMLNode("animals", .children = b)
-subTree <- newXMLNode("grp","a", "GENO_BEEF")
-
-doc = newXMLDoc()
-newXMLNode("root", parent = doc)
-
-#' https://stackoverflow.com/questions/39246686/how-to-create-xml-from-csv-properly
-xml <- xmlTree("animals")
-for (i in 1:nrow(df)) {
-  xml$addNode("a", attrs = c(id = df[i,"id"]), close = FALSE)
-#  appNames <- names(df)[names(df) != "id"]
-#  for (j in appNames) {
-#    xml$addNode(j, df[i, j])
-#  }
-  xml$closeNode()
 }
-xml$closeNode()
-saveXML(xml$doc(), "test.xml")
-xmlParse("test.xml")
+genoanim
 
 
-#' ---- ----
-#' 
-
+#' 2. Versuch
+#' Ab dem Node animals ist i.O., jedoch Interbull node fehlt
 xmlt_anim <- xmlTree("animals")
-# for (i in 1:nrow(df)) {
-  i <- 1
-  xmlt_anim$addNode("a", attrs = c(id = df[i,"id"]), close = FALSE)
-  #  appNames <- names(df)[names(df) != "id"]
-  #  for (j in appNames) {
-  #    xml$addNode(j, df[i, j])
-  #  }
-  xmlt_anim$closeNode()
-# }
-
-  xmlt_anim$doc()  
-  xmlt_anim$closeNode()  
-  saveXML(xmlt_anim$doc(), "xmlt_anim.xml")
-  xmlParse("xmlt_anim.xml")
+ for (i in 1:nrow(df)) {
+    xmlt_anim$addNode("a", attrs = c(id = df[i,"id"]), close = FALSE)
+     appNames <- names(df)[names(df) != "id"]
+     mapply(function(w, x, y, z)
+       xmlt_anim$addNode("GENO_BEEF", attrs = c(pop = w, genotyped = x, share = y, tissue = z), close = FALSE),
+       df[i,appNames[1]], df[i,appNames[2]], df[i,appNames[3]], df[i,appNames[4]])
+     xmlt_anim$closeNode()
+    xmlt_anim$closeNode()
+ }
+xmlt_anim$doc()  
   
